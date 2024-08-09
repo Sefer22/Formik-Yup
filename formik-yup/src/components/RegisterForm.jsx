73a95c2dc39f1ebf13e9fef435829eec1d1387d3
Sefer22 <sfrs87997@gmail.com>
2024-08-09 15:54:41 +0400

    first

--- formik-yup/src/components/RegisterForm.jsx
+++ formik-yup/src/components/RegisterForm.jsx
@@ -5,15 +5,37 @@ function RegisterForm() {
     const {values,errors,handleChange,handleSubmit} = useFormik({
         initialValues: {
           email: '',
+          age:'',
+          password:''
         },
       });
       console.log(formik);
   return (
     <div>
         <form>
+            <div>
             <label>Email</label>
-            <input type="text" id="email" placeholder='Enter email' 
-            value={values.email} onChange={handleChange} />
+            <input type="text" id="email" 
+            placeholder='Enter email' 
+            value={values.email} 
+            onChange={handleChange} />
+            </div>
+
+            <div>
+            <label>Age</label>
+            <input type="number" id="age" 
+            placeholder='Enter age' 
+            value={values.age} 
+            onChange={handleChange} />
+            </div>
+
+            <div>
+            <label>Password</label>
+            <input type="password" id="password" 
+            placeholder='Enter password' 
+            value={values.password} 
+            onChange={handleChange} />
+            </div>
         </form>
       
     </div>
